9c705ccdcfc2acb714018863a79b9b475a94d160
Alan Hoyte <aranhoide@gmail.com>
2016-03-27 11:56:50 +0200

    hardcode lantern user for ssh

--- src/clj/ops_panel/core.clj
+++ src/clj/ops_panel/core.clj
@@ -9,11 +9,12 @@
 
 (defn ssh [host cmd]
   (let [agent (ssh/ssh-agent {})
-        session (ssh/session agent host {:strict-host-key-checking :no})]
+        session (ssh/session agent host {:username "lantern" :strict-host-key-checking :no})]
     (ssh/with-connection session (ssh/ssh session {:cmd cmd}))))
 
 (defn pssh [hosts cmd]
-  ;;XXX: reuse pool in concurrent requests
+  ;; XXX: reuse pool in concurrent requests, maybe cache it for some time or
+  ;; even for the web server's lifetime
   (pool/pmap (min (count hosts) 50) #(ssh % cmd) hosts))
 
 (defroutes handler
