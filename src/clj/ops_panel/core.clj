5ed12be144828416b8e5fc52dc6c2dd3d72adcbb
Alan Hoyte <aranhoide@gmail.com>
2016-05-03 01:46:12 +0200

    authorization works (pending recovering CRSF protection)

--- src/clj/ops_panel/core.clj
+++ src/clj/ops_panel/core.clj
@@ -2,6 +2,8 @@
   (:require [cemerick.url :refer [url url-encode]]
             [clojure.data.json :as json]
             [clojure.pprint :as pp]
+            [clojure.stacktrace :refer [print-stack-trace]]
+            [clojure.string :as str]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [files not-found resources]]
             [environ.core :refer [env]]
@@ -10,8 +12,7 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [taoensso.sente :as sente]
             [taoensso.sente.server-adapters.http-kit :as http-kit-adapter]
-            [taoensso.sente.server-adapters.nginx-clojure :as nginx-adapter]
-            [clojure.string :as str]))
+            [taoensso.sente.server-adapters.nginx-clojure :as nginx-adapter]))
 
 (def in-development (= (env :in-development) "indeed"))
 
@@ -53,67 +54,78 @@
            ch-chsk sente-handler)))
 
 (defn root [req]
-  (if (get-in req [:session :user])
-      {:status 200
-       :headers {"content-type" "text/html"}
-       :body (html [:head [:title "ops-panel (WIP)"]]
+  (if-let [user (get-in req [:session :user])]
+    {:status 200
+     :headers {"content-type" "text/html"}
+     :body (html [:head [:title "ops-panel (WIP)"]]
+                 [:body
+                  [:h2 "Ops Panel (WIP)"]
+                  [:div (str "Hello, " user "!")]
+                  [:div "An amazing ops panel will be here Soon&trade;!"]
+                  [:div#app_container
+                   [:script {:type "text/javascript" :src "js/main.js"}]
+                   [:script {:type "text/javascript"} "ops_panel.core.main();"]]
+                  [:h2 "Your request"]
+                  [:div [:pre (with-out-str (pp/pprint req))]]])}
+    (let [redirect-url (str (assoc (url "https://github.com/login/oauth/authorize")
+                                   :query
+                                   {:client_id (env :github-client-id)
+                                    :scope "read:org"}))]
+      {:status 303
+       :headers {"content-type" "text/html"
+                 "Location" redirect-url}
+       :session (assoc (get req :session {})
+                       :github-login-original-path "/")
+       :body (html [:head [:title "Please log in"]]
                    [:body
-                    [:h2 "Ops Panel (WIP)"]
-                    [:div "An amazing ops panel will be here Soon&trade;!"]
-                    [:div#app_container
-                     [:script {:type "text/javascript" :src "js/main.js"}]
-                     [:script {:type "text/javascript"} "ops_panel.core.main();"]]
-                    [:h2 "Your request"]
-                    [:div [:pre (with-out-str (pp/pprint req))]]])}
-      (let [redirect-url (str (assoc (url "https://github.com/login/oauth/authorize")
-                                     :query
-                                     {:client_id (env :github-client-id)
-                                      :scope "read:org"}))]
-        {:status 303
-         :headers {"content-type" "text/html"
-                   "Location" redirect-url}
-         :session (assoc (get req :session {})
-                         :github-login-original-path "/")
-         :body (html [:head [:title "Please log in"]]
-                     [:body
-                      [:h2 "Please log in"]
-                      [:p "Redirecting you to " redirect-url " to log in."]])})))
+                    [:h2 "Please log in"]
+                    [:p "Redirecting you to " redirect-url " to log in."]])})))
 
-(defn github-auth-cb [code req]
+(defn github-auth-cb- [code req]
   (let [token-resp @(http/post "https://github.com/login/oauth/access_token"
                                {:headers {"Accept" "application/json"}
                                 :form-params {:client_id (env :github-client-id)
                                               :client_secret (env :github-client-secret)
                                               :code code}})
         token-map (json/read-str (:body token-resp))
-        scope (str/split (get token-map "scope"))
         access-token (get token-map "access_token")
-;        orgs-resp @(http/get "https://api.github.com/")
-        user-resp @(http/get "https://api.github.com/user"
+        orgs-resp @(http/get "https://api.github.com/user/orgs"
                              {:headers {"Accept" "application/json"
                                         "Authorization" (str "token " access-token)}})
-        user (get (json/read-str (:body user-resp))
-                  "login")
-        original-path (get-in req [:session :github-login-original-path] "/")]
-    (println "client_secret" (env :github-client-secret))
-    (println "code" code)
-    (println "req" req)
-    (println "token-resp" token-resp)
-    (println "body" (:body token-resp))
-    (println "body-at" (get (:body token-resp) "access_token"))
-    (println "loaded-body" (json/read-str (:body token-resp)))
-    (println "loaded-body-at" (get (json/read-str (:body token-resp)) "access_token"))
-    (println "access-token" access-token)
-    (println "user-resp" user-resp)
-    (println "user" user)
-    {:status 303
-     :headers {"content-type" "text/html"
-               "location" original-path}
-     :session (assoc (get req :session {}) :user (str user))
-     :body (html [:head [:title "Login successful"]]
-                 [:body [:h1 "Login successful"]
-                  [:p "Redirecting you to "
-                   [:a {:href original-path} original-path]]])}))
+        authorized (and (= (:status orgs-resp) 200)
+                        (some #{"getlantern"}
+                              (for [org (json/read-str (:body orgs-resp))]
+                                (get org "login"))))]
+    (if authorized
+      (let [user-resp @(http/get "https://api.github.com/user"
+                                 {:headers {"Accept" "application/json"
+                                            "Authorization" (str "token " access-token)}})
+            user (get (json/read-str (:body user-resp))
+                      "login")
+            original-path (get-in req [:session :github-login-original-path] "/")]
+        {:status 303
+         :headers {"content-type" "text/html"
+                   "location" original-path}
+         :session (assoc (get req :session {}) :user (str user))
+         :body (html [:head [:title "Login successful"]]
+                     [:body [:h1 "Login successful"]
+                      [:p "Redirecting you to "
+                       [:a {:href original-path} original-path]]])})
+      (throw (RuntimeException. "Not in getlantern group")))))
+
+(defn github-auth-cb [code req]
+  ;; This is an internal tool, I don't expect this to fail a lot, and I expect a
+  ;; page refresh to fix it 99% of the time, so tracebacks in logs will do for
+  ;; error reporting in this MVP until we find out we need more.
+  (try (github-auth-cb- code req)
+       (catch Exception e
+         (print-stack-trace e 40)
+         (flush)
+         {:status 403
+          :headers {"content-type" "text/html"}
+          :body (html [:head [:title "Not authorized"]]
+                      [:body [:h2 "Not authorized"]
+                       [:div "Only members of the getlantern github organization are allowed to access this page."]])})))
 
 (comment  [{{:keys [code state]} :params
             {session-state :state} :session
