ae18eecc8f66a05a61dcfe2bd1a44b1dc596c7aa
Alan Hoyte <aranhoide@gmail.com>
2016-05-03 03:18:22 +0200

    move github auth to own source file
    Also start to standardize on round parens for :refer clauses.

--- src/clj/ops_panel/core.clj
+++ src/clj/ops_panel/core.clj
@@ -1,16 +1,13 @@
+;;; Compojure and Sente routing.
+
 (ns ops-panel.core
-  (:require [cemerick.url :refer [url url-encode]]
-            [clojure.data.json :as json]
-            [clojure.pprint :as pp]
-            [clojure.stacktrace :refer [print-stack-trace]]
-            [clojure.string :as str]
-            [compojure.core :refer [defroutes GET POST]]
-            [compojure.route :refer [files not-found resources]]
-            crypto.random
-            [environ.core :refer [env]]
-            [hiccup.core :refer [html]]
-            [org.httpkit.client :as http]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+  (:require [clojure.pprint :as pp]
+            [compojure.core :refer (defroutes GET POST)]
+            [compojure.route :refer (files not-found resources)]
+            [environ.core :refer (env)]
+            [hiccup.core :refer (html)]
+            [ops-panel.github-login :as github-login]
+            [ring.middleware.defaults :refer (wrap-defaults site-defaults)]
             [taoensso.sente :as sente]
             [taoensso.sente.server-adapters.http-kit :as http-kit-adapter]
             [taoensso.sente.server-adapters.nginx-clojure :as nginx-adapter]))
@@ -68,86 +65,13 @@
                    [:script {:type "text/javascript"} "ops_panel.core.main();"]]
                   [:h2 "Your request"]
                   [:div [:pre (with-out-str (pp/pprint req))]]])}
-    (let [csrf-token (crypto.random/url-part 64)
-          redirect-url (str (assoc (url "https://github.com/login/oauth/authorize")
-                                   :query
-                                   {:client_id (env :github-client-id)
-                                    :scope "read:org"
-                                    :state csrf-token}))]
-      {:status 303
-       :headers {"content-type" "text/html"
-                 "Location" redirect-url}
-       :session (assoc (get req :session {})
-                       :github-login-original-path "/"
-                       :github-csrf-token csrf-token)
-       :body (html [:head [:title "Please log in"]]
-                   [:body
-                    [:h2 "Please log in"]
-                    [:p "Redirecting you to " redirect-url " to log in."]])})))
-
-(defn auth-error [reason]
-  (throw (RuntimeException. reason)))
-
-(defn github-auth-cb- [code csrf-token session]
-  (let [session-csrf-token (:github-csrf-token session)]
-    (cond
-      (not session-csrf-token) (auth-error "No CSRF token in session")
-      (not (= csrf-token session-csrf-token)) (auth-error "CSRF token mismatch")
-      :else
-      (let [token-resp @(http/post "https://github.com/login/oauth/access_token"
-                                   {:headers {"Accept" "application/json"}
-                                    :form-params {:client_id (env :github-client-id)
-                                                  :client_secret (env :github-client-secret)
-                                                  :code code}})
-            token-map (json/read-str (:body token-resp))
-            access-token (get token-map "access_token")
-            orgs-resp @(http/get "https://api.github.com/user/orgs"
-                                 {:headers {"Accept" "application/json"
-                                            "Authorization" (str "token " access-token)}})
-            authorized (and (= (:status orgs-resp) 200)
-                            (some #{"getlantern"}
-                                  (for [org (json/read-str (:body orgs-resp))]
-                                    (get org "login"))))]
-        (if-not authorized
-          (auth-error "Not in getlantern group")
-          (let [user-resp @(http/get "https://api.github.com/user"
-                                     {:headers {"Accept" "application/json"
-                                                "Authorization" (str "token " access-token)}})
-                user (get (json/read-str (:body user-resp))
-                          "login")
-                original-path (get session :github-login-original-path "/")]
-            {:status 303
-             :headers {"content-type" "text/html"
-                       "location" original-path}
-             :session (assoc session :user (str user))
-             :body (html [:head [:title "Login successful"]]
-                         [:body [:h1 "Login successful"]
-                          [:p "Redirecting you to "
-                           [:a {:href original-path} original-path]]])}))))))
-
-(defn github-auth-cb [code csrf-token session]
-  ;; This is an internal tool, I don't expect this to fail a lot, and I expect a
-  ;; page refresh to fix it 99% of the time, so tracebacks in logs will do for
-  ;; error reporting in this MVP until we find out we need more.
-  (try (github-auth-cb- code csrf-token session)
-       (catch Exception e
-         (print-stack-trace e 40)
-         (flush)
-         {:status 403
-          :headers {"content-type" "text/html"}
-          :body (html [:head [:title "Not authorized"]]
-                      [:body [:h2 "Not authorized"]
-                       [:div "Only members of the getlantern github organization are allowed to access this page."]])})))
-
-(comment  [{{:keys [code state]} :params
-            {session-state :state} :session
-            :as req}])
+    (github-login/login req)))
 
 (defroutes handler
 
   (GET "/" req (root req))
   (GET "/github-auth-cb" [code state :as req]
-    (github-auth-cb code state (get req :session {})))
+    (github-login/github-auth-cb code state (get req :session {})))
   ;; sente
   (GET  "/chsk" req (ring-ajax-get-or-ws-handshake req))
   (POST "/chsk" req (ring-ajax-post req))
